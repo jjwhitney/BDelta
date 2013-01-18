a936fed8423e7b6f665e86f632fb95370c86a4e2
John Whitney <jjw@emsoftware.com>
2013-01-18 15:24:00 -0600

    Automatically pick version based on git repository

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
+from version import get_git_version
 
 ext_modules = [Extension(
 	"bdelta",
@@ -9,12 +10,11 @@ ext_modules = [Extension(
 
 setup(
     name = 'BDelta',
-    version='0.3.0',
-    description='Python Bindings for BDelta',
-    author='John Whitney',
-    author_email='jjw@deltup.org',
-    url='http://deltup.org',
+    version = get_git_version(),
+    description = 'Python Bindings for BDelta',
+    author = 'John Whitney',
+    author_email = 'jjw@deltup.org',
+    url = 'http://bdelta.org',
     cmdclass = {'build_ext': build_ext},
     ext_modules = ext_modules
 )
-
