53e49e1989fbc5bed12865a6dcda1e3050627249
John Whitney <jjw@emsoftware.com>
2013-01-29 22:42:30 -0600

    Enable exceptions in Windows for STL ("/EHsc")

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
 from version import get_git_version
+import sys
 
-ext_modules = [Extension(
+bdelta_module = Extension(
 	"bdelta",
 	["src/bdelta.pyx", "src/libbdelta.cpp"],
-	define_macros=[('TOKEN_SIZE', '2')],
-)]
+	define_macros = [('TOKEN_SIZE', '2')],
+	extra_compile_args = ['/EHsc'] if sys.platform == 'win32' else None
+)
 
 setup(
     name = 'BDelta',
@@ -16,5 +18,5 @@ setup(
     author_email = 'jjw@deltup.org',
     url = 'http://bdelta.org',
     cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules
+    ext_modules = [bdelta_module]
 )
